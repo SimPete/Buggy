f399ae948bd148364ae09a80d6c634b6b0cd2122
SimPete <cote.simon.pierre@gmail.com>
2019-04-28 15:57:06 +0000

    Syntax errors bug fixes

--- motor.py
+++ motor.py
@@ -1,15 +1,17 @@
-# Script pour contrôler deux moteurs en Python. 
+# Script pour controler deux moteurs en Python. 
 
 # Import required modules
 import time
 import RPi.GPIO as GPIO
 import sys
-import stdin
+# import stdin
 
 # Declare the GPIO settings
+print "Setting GPIO mode to BOARD"
 GPIO.setmode(GPIO.BOARD)
 
 # set up GPIO pins
+print "Setting up GPIO pins to OUTPUT"
 GPIO.setup(7, GPIO.OUT) # Connected to PWMA
 GPIO.setup(11, GPIO.OUT) # Connected to AIN2
 GPIO.setup(12, GPIO.OUT) # Connected to AIN1
@@ -18,18 +20,20 @@ GPIO.setup(16, GPIO.OUT) # Connected to BIN2
 GPIO.setup(18, GPIO.OUT) # Connected to PWMB
 
 # Set all the GPIO pins by setting them to LOW
+print "Setting up GPIO pins to LOW"
 GPIO.output(12, GPIO.LOW) # Set AIN1
 GPIO.output(11, GPIO.LOW) # Set AIN2
 GPIO.output(7, GPIO.LOW) # Set PWMA
-GPIO.output(13, GPIO.LOW) # Set STBY
 GPIO.output(15, GPIO.LOW) # Set BIN1
 GPIO.output(16, GPIO.LOW) # Set BIN2
 GPIO.output(18, GPIO.LOW) # Set PWMB
 
-# def getch(): #FONCTION À VÉRIFIER (https://www.instructables.com/id/Controlling-a-Raspberry-Pi-RC-Car-With-a-Keyboard/)
+# def getch(): #FONCTION A VERIFIER (https://www.instructables.com/id/Controlling-a-Raspberry-Pi-RC-Car-With-a-Keyboard/)
 #    fd = sys.stdin.fileno()
 #    ch = sys.stdin.read(1)
 #    return ch
+
+print "Entering the dreaded DEF zone"
 
 def MotorA_FWD():
 	GPIO.output(12, GPIO.HIGH)
@@ -63,16 +67,16 @@ def cleanup():
 	GPIO.output(12, GPIO.LOW) # Set AIN1
 	GPIO.output(11, GPIO.LOW) # Set AIN2
 	GPIO.output(7, GPIO.LOW) # Set PWMA
-	GPIO.output(13, GPIO.LOW) # Set STBY
 	GPIO.output(15, GPIO.LOW) # Set BIN1
 	GPIO.output(16, GPIO.LOW) # Set BIN2
 	GPIO.output(18, GPIO.LOW) # Set PWMB
 	GPIO.cleanup()
 	print "GPIO are clean!"
 
-def #main()
+def main():
 #TODO MainFunction with keyboard controls, add exit criteria
-	try:  
+        try:  
+                print "We're in the game now!"
 		# Drive the motor clockwise
 		# Motor A:
 		GPIO.output(12, GPIO.HIGH) # Set AIN1
@@ -85,11 +89,8 @@ def #main()
 		# Motor A:
 		GPIO.output(7, GPIO.HIGH) # Set PWMA
 		# Motor B:
-		GPIO.output(18, GPIO.HIGH) # Set PWMB
-		
-		# Disable STBY (standby)
-		GPIO.output(13, GPIO.HIGH)
-		
+	 	GPIO.output(18, GPIO.HIGH) # Set PWMB
+				
 		# Wait 5 seconds
 		time.sleep(5)
 		
@@ -106,9 +107,6 @@ def #main()
 		GPIO.output(7, GPIO.HIGH) # Set PWMA
 		# Motor B:
 		GPIO.output(18, GPIO.HIGH) # Set PWMB
-	
-		# Disable STBY (standby)
-		GPIO.output(13, GPIO.HIGH)
 		
 		# Wait 5 seconds
 		time.sleep(5)
@@ -125,13 +123,10 @@ def #main()
 		print "Other error or exception occurred!"  
 
 	finally:  
+                print "We're in the End Game now!"
 		cleanup() # GPIO cleanup on exit
-
-		
-
-if __name__ == '__main__'
+	
+if __name__ == "__main__":
 	main()
   
-GPIO.cleanup() # Second cleanup... just in case
-print "Cleaned up a second time... without the function... just in case! The program is done now!" 
 print "Job's done!"
